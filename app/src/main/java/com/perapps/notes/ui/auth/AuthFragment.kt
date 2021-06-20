efefdefa4cc5061cc700f0b2e5c16113d30c3446
Rawat2510 <prashantrawat659@gmail.com>
2021-06-20 19:38:25 +0530

    internet connectivity check added

--- app/src/main/java/com/perapps/notes/ui/auth/AuthFragment.kt
+++ app/src/main/java/com/perapps/notes/ui/auth/AuthFragment.kt
@@ -1,17 +1,61 @@
 package com.perapps.notes.ui.auth
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import com.perapps.notes.R
+import com.perapps.notes.databinding.FragmentAuthBinding
 import com.perapps.notes.ui.BaseFragment
 
 class AuthFragment : BaseFragment(R.layout.fragment_auth) {
 
+    private var _binding: FragmentAuthBinding? = null
+    private val binding get() = _binding!!
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+        _binding = FragmentAuthBinding.bind(view)
+        setListeners()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setListeners() {
+        binding.btnSignIn.setOnTouchListener { _, motionEvent ->
+            if(motionEvent.action == MotionEvent.ACTION_UP){
+                signUp()
+            }
+            false
+        }
+        binding.btnRegister.setOnTouchListener { _, motionEvent ->
+            if(motionEvent.action == MotionEvent.ACTION_UP){
+                register()
+            }
+            false
+        }
+    }
+
+    private fun register(){
+        if (binding.tietEmail.text.toString()
+                .isNotBlank() && binding.tietPassword.text.toString().isNotBlank()
+        ){
+            navController.navigate(AuthFragmentDirections.actionAuthFragmentToNotesFragment())
+        }
+    }
+
+    private fun signUp(){
+        if (binding.tietEmail.text.toString()
+                .isNotBlank() && binding.tietPassword.text.toString().isNotBlank()
+        ){
+            navController.navigate(AuthFragmentDirections.actionAuthFragmentToNotesFragment())
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
